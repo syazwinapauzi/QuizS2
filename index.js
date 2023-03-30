aa4374f301d13711ac20b01c07f8eab6fdea2b31
syazwinapauzi <norsyazwina_1507@gmail.com>
2023-03-30 15:53:05 +0800

    message

---
+++ index.js
@@ -0,0 +1,42 @@
+let message = "benr" //message is a variable  //variable can change in any data types
+//console.log(message) 
+//console.log(typeof message) //to know the variable data types
+
+message = 123 //declare message is 123
+//console.log(message, typeof message)
+let str = "1.2"
+let num = 1
+//in java can add string and number
+let total = str + num //parseInt and parseFloat depends on the types of string number
+//console.log(total, typeof total)
+
+let data = ["benr","3533","true"]
+//console.log(data[0])
+//console.log(data[1])
+//console.log(data[2])
+
+let user = {
+    name: "awin",
+    age: 23,
+    faculty: "FKEKK"
+}
+//console.log(user.name)
+//console.log(user.age)
+//console.log(user.faculty)
+
+let messages = [
+    {
+        name: "usr1",
+        date: "1/4/2023",
+        msg: "aju johayo",
+        like: 30
+
+    },
+    {
+        name: "usr2",
+        date: "2/4/2023",
+        msg: "kkukkukkakka",
+        like: 30000
+    },
+]
+console.log(messages[1].msg)
